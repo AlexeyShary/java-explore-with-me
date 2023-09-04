e5b639c854e77872d275f0b3cf99a0734432aef3
Alexey Shary <badirbis@yandex.ru>
2023-09-04 10:30:01 +0300

    refactor: JsonFormat for EndpointHitDto

--- stats/dto/src/main/java/ru/practicum/ewm/stats/dto/EndpointHitDto.java
+++ stats/dto/src/main/java/ru/practicum/ewm/stats/dto/EndpointHitDto.java
@@ -1,9 +1,13 @@
 package ru.practicum.ewm.stats.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -14,6 +18,8 @@ public class EndpointHitDto {
     private String uri;
     @NotBlank
     private String ip;
-    @NotBlank
-    private String timestamp;
+    @NotNull
+    @JsonProperty("timestamp")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime hitTimestamp;
 }
--- stats/server/src/main/java/ru/practicum/ewm/stats/server/data/StatsMapper.java
+++ stats/server/src/main/java/ru/practicum/ewm/stats/server/data/StatsMapper.java
@@ -3,19 +3,14 @@ package ru.practicum.ewm.stats.server.data;
 import lombok.experimental.UtilityClass;
 import ru.practicum.ewm.stats.dto.EndpointHitDto;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @UtilityClass
 public class StatsMapper {
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
     public EndpointHitDto toDto(EndpointHit entity) {
         return EndpointHitDto.builder()
                 .app(entity.getApp())
                 .uri(entity.getUri())
                 .ip(entity.getIp())
-                .timestamp(entity.getTimestamp().format(DATE_TIME_FORMATTER))
+                .hitTimestamp(entity.getTimestamp())
                 .build();
     }
 
@@ -24,7 +19,7 @@ public class StatsMapper {
         entity.setApp(dto.getApp());
         entity.setUri(dto.getUri());
         entity.setIp(dto.getIp());
-        entity.setTimestamp(LocalDateTime.parse(dto.getTimestamp(), DATE_TIME_FORMATTER));
+        entity.setTimestamp(dto.getHitTimestamp());
         return entity;
     }
 }
