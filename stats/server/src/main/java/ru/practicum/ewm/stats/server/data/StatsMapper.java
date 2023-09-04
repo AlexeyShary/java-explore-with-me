af48582dbbb8531056dc95a66d594519d7f479a0
Alexey Shary <badirbis@yandex.ru>
2023-09-04 09:51:29 +0300

    refactor: add @UtilityClass to StatsMapper

--- stats/server/src/main/java/ru/practicum/ewm/stats/server/data/StatsMapper.java
+++ stats/server/src/main/java/ru/practicum/ewm/stats/server/data/StatsMapper.java
@@ -1,14 +1,16 @@
 package ru.practicum.ewm.stats.server.data;
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.ewm.stats.dto.EndpointHitDto;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@UtilityClass
 public class StatsMapper {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static EndpointHitDto toDto(EndpointHit entity) {
+    public EndpointHitDto toDto(EndpointHit entity) {
         return EndpointHitDto.builder()
                 .app(entity.getApp())
                 .uri(entity.getUri())
@@ -17,7 +19,7 @@ public class StatsMapper {
                 .build();
     }
 
-    public static EndpointHit fromDto(EndpointHitDto dto) {
+    public EndpointHit fromDto(EndpointHitDto dto) {
         EndpointHit entity = new EndpointHit();
         entity.setApp(dto.getApp());
         entity.setUri(dto.getUri());
